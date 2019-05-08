713b3ad6a7dc661b762349d1023e2d4470abd2e8
marinasavluchinska <anauk2018@gmail.com>
2019-05-08 14:05:59 +0300

    actions in counter

--- src/actions.js
+++ src/actions.js
@@ -3,4 +3,10 @@ export const inc =() =>{
     return {type:'INC'};
 };
 export const dec = () => ({type:'DEC'});
-export const rnd=(payload)=>({type:'RND', payload});
+export const rnd=()=>{
+    const payload= Math.floor(Math.random() * 10);
+    return {
+        type:'RND',
+        payload
+    };
+}
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
-const Counter=({counter, inc, dec,rnd})=>{
+import * as actions from "../actions";
+import { bindActionCreators} from 'redux';
+
+const Counter = ({counter, inc, dec, rnd}) => {
     return (
         <div className="jumbotron">
-            <h2 >{counter}</h2>
+            <h2>{counter}</h2>
             <button
                 onClick={inc}
-                className="btn btn-primary btn-lg">INC</button>
+                className="btn btn-primary btn-lg">INC
+            </button>
             <button
                 onClick={dec}
-                className="btn btn-primary btn-lg">DEC</button>
+                className="btn btn-primary btn-lg">DEC
+            </button>
             <button
                 onClick={rnd}
-                className="btn btn-primary btn-lg">RND</button>
+                className="btn btn-primary btn-lg">RND
+            </button>
         </div>
     );
 };
-const mapStateToProps = (state)=>{
+const mapStateToProps = (state) => {
     return {
         counter: state
-    }
-}
-export default connect(mapStateToProps)(Counter);
+    };
+};
+
+export default connect(mapStateToProps, actions)(Counter);
