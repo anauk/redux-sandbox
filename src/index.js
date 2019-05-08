1363d88b29a396e4da51eff81193582b5f406995
marinasavluchinska <anauk2018@gmail.com>
2019-05-08 11:06:20 +0300

    counter on redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ const reducer =(state=0, action)=>{
 
     switch(action.type){
         case 'INC': return state+1;
+        case 'DEC': return state-1;
         default: return state;
     }
 };
 const store = createStore(reducer);
-store.subscribe(()=>{
-    console.log(store.getState(), 'subscribe');
-})
-console.log(store.getState(),'console1');
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-console.log(store.getState(),'console2');
+document.getElementById('inc')
+.addEventListener('click', ()=>{
+    store.dispatch({type:'INC'});
+});
+document.getElementById('dec')
+.addEventListener('click', ()=> {
+    store.dispatch({type: 'DEC'});
+});
+const update =() =>{
+document.getElementById('counter')
+    .innerHTML = store.getState();
+};
+store.subscribe(update);
